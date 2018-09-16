1c5d609fdcea3c373e0aec695b4550cfde0734d8
MinKyu Kim <tomorDeveloper@gmail.com>
2018-09-17 01:22:56 +0900

    github pages에서 열리도록 시도(2)

--- TodoApplication/ts/main.ts
+++ TodoApplication/ts/main.ts
@@ -27,5 +27,23 @@ class Main {
                 new TodoApp(this.jul8, this.appNode);
             });
         });
+
+        // jul8config.json을 읽어 존재하는 템플릿을 확인합니다.
+        $.get("../jul8config.json", (config: any) => {
+
+            // Jul8-Quick-Start에서는 하나의 템플릿만 사용하여 흐름을 파악합니다.
+            let templatePath: string = config.build[0].source;
+
+            $.get(templatePath, (data) => {
+
+                let virtualDOM: DocumentFragment = document.createDocumentFragment();
+                $(virtualDOM).append(data);
+
+                this.jul8 = new Jul8.TemplateHolder();
+                this.jul8.addTemplateRoot($(virtualDOM));
+
+                new TodoApp(this.jul8, this.appNode);
+            });
+        });
     }
 }
